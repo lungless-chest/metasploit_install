6caa48213eb043533d7a3273feac675a80acd4d8
Sage <148996861+lungless-chest@users.noreply.github.com>
2024-02-16 14:53:16 -0700

    Update metasploit_install.sh

--- metasploit_install.sh
+++ metasploit_install.sh
@@ -1,11 +1,13 @@
  sudo apt update
- sudo apt upgrade
+ sudo apt-get install pip
+ pip install shodan
  sudo apt-get install build-essential zlib1g zlib1g-dev libxml2 libxml2-dev libxslt-dev locate libreadline6-dev libcurl4-openssl-dev git-core autoconf curl postgresql postgresql-contrib libpq-dev libapr1 libaprutil1 libsvn1 libpcap-dev
 git clone https://github.com/rapid7/metasploit-framework.git
+ sudo snap install metasploit-framework
  cd metasploit-framework
  sudo bash -c 'for MSF in $(ls msf*); do ln -s /usr/local/src/metasploit-framework/$MSF /usr/local/bin/$MSF;done'
- sudo service postgresql start
  cd ~
- sudo snap install metasploit-framework
+ sudo service postgresql start
  msfdb init
+ sudo apt upgrade -y
  msfconsole
