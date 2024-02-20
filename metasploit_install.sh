9860ba07190cad0649a41ee6638a0fc6e2555378
Sage <148996861+lungless-chest@users.noreply.github.com>
2024-02-20 14:49:25 -0700

    Update metasploit_install.sh

--- metasploit_install.sh
+++ metasploit_install.sh
@@ -3,7 +3,7 @@
  sudo apt-get install -y build-essential zlib1g zlib1g-dev libxml2 libxml2-dev libxslt-dev locate libreadline6-dev libcurl4-openssl-dev git-core autoconf curl postgresql postgresql-contrib libpq-dev libapr1 libaprutil1 libsvn1 libpcap-dev
  cd /usr/share
  sudo snap install -y metasploit-framework
- git clone https://github.com/rapid7/metasploit-framework.git
+ sudo git clone https://github.com/rapid7/metasploit-framework.git
  cd metasploit-framework
  sudo bash -c 'for MSF in $(ls msf*); do ln -s /usr/local/src/metasploit-framework/$MSF /usr/local/bin/$MSF;done'
  sudo service postgresql start
